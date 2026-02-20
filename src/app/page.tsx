5d9b809da1fbcf21fa2de7749629938758c761cd
Ferry <ferryyurri@gmail.com>
2026-02-20 18:11:32 +0000

    Fix: simplify page, remove Convex query for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import { useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const events = useQuery(api.events.list);
+  const events = [];
 
 export default function Home() {
   return (
